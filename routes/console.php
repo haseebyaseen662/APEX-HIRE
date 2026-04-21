fcc7f81e602f85e83ddc7200829bd63c4b593348
Muhammad Haseeb <haseebreignx@gmail.com>
2026-04-21 11:50:06 +0500

    cron jobs setup

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Console\Scheduling\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('queue:work --stop-when-empty')
+    ->everyMinute()
+    ->withoutOverlapping(10) // Prevents multiple workers from running simultaneously
+    ->runInBackground()
+    ->onSuccess(function () {
+        Log::info('Queue worker finished successfully.');
+    })
+    ->onFailure(function () {
+        Log::error('Queue worker failed.');
+    });
