28848c01403e94143fdf4299908f32521cfe913e
Muhammad Haseeb <haseebreignx@gmail.com>
2026-05-11 21:16:15 +0500

    Resend Integration Bug Fixed

--- app/Listeners/SendWelcomeEmail.php
+++ app/Listeners/SendWelcomeEmail.php
@@ -3,17 +3,16 @@
 namespace App\Listeners;
 
 use App\Mail\WaitlistJoinedMail;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
 use App\Events\WaitlistJoinedEvent;
 
-class SendWelcomeEmail implements ShouldQueue
+class SendWelcomeEmail
 {
     public function __construct() {}
 
     public function handle(WaitlistJoinedEvent $event): void
     {
-        Mail::to($event->waitlist->email)->queue(new WaitlistJoinedMail($event->waitlist));
+        Mail::to($event->waitlist->email)->send(new WaitlistJoinedMail($event->waitlist));
     }
 }
 
